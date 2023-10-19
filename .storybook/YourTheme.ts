eba860b1fc8bb30ff5a8c7c79475f27300d3b3d0
Lewis <lewis.nguyen@terralogic.com>
2023-10-19 16:54:19 +0700

    Update: Change storybook theme

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "ZENI-UI",
+  brandTarget: "_self",
+  appBg: "#F6F4EB",
+  appBorderRadius: 8,
+});
