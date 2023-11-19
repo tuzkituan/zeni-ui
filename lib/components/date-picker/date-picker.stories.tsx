12a8b207fc7b5363aa6fc2dbce9e5488ceb8be7e
tuannguyenitpy <tuannguyenitpy@gmail.com>
2023-11-19 05:45:16 +0000

    Fix: build errors

--- lib/components/date-picker/date-picker.stories.tsx
+++ lib/components/date-picker/date-picker.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 // import { isBefore } from "date-fns";
 import { DatePicker } from "./date-picker";
-import { isBefore } from "date-fns";
 
 const meta = {
   title: "FORMS/DatePicker",
